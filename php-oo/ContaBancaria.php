284a8861be423987265f7c2b90c35f962590c4e5
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-21 19:20:16 -0400

    criado funções depositar, sacar

--- php-oo/ContaBancaria.php
+++ php-oo/ContaBancaria.php
@@ -1,15 +1,40 @@
 <?php 
 
+declare(Strict_types=1);
+
 class ContaBancaria 
 {
-  public $banco;
-  public $nomeTitular;
-  public $numeroAgencia;
-  public $numeroConta;
-  public $saldo;
+  private $banco;
+  /**
+   * @var string
+   */
+  private $nomeTitular;
+  /**
+   * @var string
+   */
+  private $numeroAgencia;
+  /**
+   * @var string
+   */
+  private $numeroConta;
+  /**
+   * @var string
+   */
+  private $saldo;
+  /**
+   * @var float
+   */
 
-  public function _construct($banco, $nomeTitular, $numeroAgencia, $numeroConta, $saldo) 
+  public function _construct(
+    string $banco, 
+    string $nomeTitular, 
+    string $numeroAgencia, 
+    string $numeroConta, 
+    float $saldo
+    
+  )
   {
+
     $this -> banco = $banco;
     $this -> nomeTitular = $nomeTitular;
     $this -> numeroAgencia = $numeroAgencia;
@@ -17,12 +42,23 @@ class ContaBancaria
     $this -> saldo = $saldo;
   }
 
-  public function obterSaldo() 
+  public function obterSaldo() : string 
   {
 
-    return 'Olá, eu sou o método obterSaldo()';
+    return 'Seu saldo atual é R$: ' . $this -> saldo;
 
   }
+   public function depositar(float $valor) : string 
+   {
+     $this -> saldo += $valor;
+     return 'Depósito de R$ ' . $valor . 'realizado!';
+   }
+
+   public function sacar(float $valor) : string 
+   {
+     $this -> saldo -= $valor;
+     return 'Saque de R$ ' . $valor . 'realizado!';
+   }
 }
 
 $conta = new ContaBancaria(
@@ -31,7 +67,26 @@ $conta = new ContaBancaria(
   'Wandwerson Timóteo', // nomeTitular
   '1534', // numeroAgencia
   '16943-1', // numeroConta
-  400.00 // saldo
+  0 // saldo
 );
 
-var_dump($conta);
+//var_dump($conta);
+echo $conta -> obterSaldo(); // 0
+
+echo PHP_EOL;
+
+echo $conta -> depositar(300.00);
+
+echo PHP_EOL;
+
+echo $conta -> obterSaldo(); // 300.00
+
+echo PHP_EOL;
+
+echo $conta -> sacar(150.00);
+
+echo PHP_EOL;
+
+echo $conta -> obterSaldo(); // 150.00
+
+echo PHP_EOL;
