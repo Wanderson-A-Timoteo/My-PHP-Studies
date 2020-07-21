ae123e308a351c005b548331d8590caf20e82368
Wanderson-A-Timoteo <wanderson_timoteo@hotmail.com>
2020-07-21 19:50:52 -0400

    criado file excecao01.php como exemplo de erro

---
+++ php-oo/excecoes01.php
@@ -0,0 +1,9 @@
+<?php
+
+declare(strict_types=1);
+
+// Essa exceção dará erro pois não foi tratada, apenas como exemplo
+
+throw new Exception("Essa é uma exceção!");
+
+echo "\n... executando ...\n";
