a8079075b6447535a1a7878d788bc2e493b05e12
Wanderson A. Timoteo <wanderson_timoteo@hotmail.com>
2021-07-05 20:07:58 -0400

    Corrigido o array lista, retirando as declarações de variaveis, corrigindo apenas sintaxe

--- B7WEB/PHP/Modulo01/Exercicio-01/index.php
+++ B7WEB/PHP/Modulo01/Exercicio-01/index.php
@@ -20,22 +20,17 @@ echo "VIDA: ".$lista['vida'];
 -->
 <?php
 $lista = [
-  'nome' => 'Bonieky',
-  'idade' => 90,
-  'atributos' => [
-      'forca' => 60,
-      'agilidade' => 80,
-      'destreza' => 50,
+  "nome" => "Boniecky",
+  "idade" => 90,
+  "atributos" => [
+      "forca" => 60,
+      "agilidade" => 80,
+      "destreza" => 50
   ],
-  // vida e mana estão dentro do array lista, porém sem a sintaxe correta, precisamos declara-las como uma variavel.
-  $vida = 1000,
-  $mana = 928,
+  // vida e mana estão dentro do array lista, porém sem a sintaxe correta
+  "vida" => 1000,
+  "mana" => 928
 ];
-
-echo "NOME: " .$lista['nome']."<br/>";
-echo "FORÇA: ".$lista['atributos']['forca']."<br/>";
-  // Devido $vida ser uma variavel declarada dentro do array, precisamos imprimir ela direto sem percorrer a lista. 
-echo "VIDA: ".$vida;
-
-?>
-
+echo "NOME: ".$lista["nome"]."<br/>";
+echo "FORÇA: ".$lista["atributos"]["forca"]."<br/>";
+echo "VIDA: ".$lista["vida"];
