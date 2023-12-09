7d0c9223723729722f9b5cd1c39355784daa40e6
Wanderson A. Timóteo <wanderson_timoteo@hotmail.com>
2023-12-09 19:39:47 -0400

    add exemplo Closure, toda função anonima é uma  Closure que pode receber uma variavel de escopo externo ou não

--- php-funcional/src/brincadeira.php
+++ php-funcional/src/brincadeira.php
@@ -1,12 +1,18 @@
 <?php
+
+$varialvel = 'teste';
+
 function outra(callable $funcao): void
 {
   echo "Executando outra função: ";
   echo $funcao();
 }
 
-$nomeDaFuncao = function () {
+$nomeDaFuncao = function () use ($varialvel) {
+  echo $varialvel;
   return "Olá, eu sou uma função anônima!";
 };
 
 outra($nomeDaFuncao);
+
+var_dump($nomeDaFuncao);
